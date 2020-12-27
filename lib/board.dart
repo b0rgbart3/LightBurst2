846b0119e9b83ec589f188dcb4e474d16687fe72
Bart Dority <b0rgBart3@gmail.com>
2020-12-27 12:12:57 -0800

    Child and Parent communicationgit add .

--- lib/board.dart
+++ lib/board.dart
@@ -4,6 +4,8 @@ import 'box.dart';
 import 'sequence.dart';
 import 'dart:developer' as developer;
 
+
+
 class Board extends StatefulWidget {
   Board({Key key, this.title}) : super(key: key);
   final String title;
@@ -13,56 +15,88 @@ class Board extends StatefulWidget {
 }
 
 class _BoardState extends State<Board> {
+
  // int _counter = 0;
   List boardList = [];
   List tileList = [];
+  List keyList = [];
   bool tilesCreated = false;
   int tileCount = 5;
-  int sequenceLength = 10;
+  int sequenceLength = 4;
   Sequence _sequence;
+  
+
+
+@override
+void initState() {
+  super.initState();
+  clearBoard();
+}
+  
+
 
   void clearBoard() {
     for (var i = 0; i < tileCount * tileCount; i++) {
       boardList.add(false);
     }
+    developer.log("CREATING NEW SEQUENCE.");
+    _sequence = new Sequence(sequenceLength, tileCount);
+    _sequence.generateRandomSequence(sequenceLength);
+    developer.log('SEQUENCE: ' + _sequence.touches.toString());
+    //developer.log(boardList.toString());
+    _sequence.touches.forEach((ID) => 
+       touchTile(ID)
+    );
   }
   void touchTile( tileID ) {
-    // developer.log(tileID.toString());
+     developer.log("In TouchTile: " + tileID.toString());
     var index = tileID['row'] * tileCount + tileID['col'];
     // developer.log('Index: ' + index.toString());
-    toggleTile(index);
+    toggleTile( tileID );
 
     //above
-    var above = tileID['row'] -1;
-    if (above >= 0) { 
-      // developer.log(above.toString());
-      toggleTile(  above * tileCount + tileID['col'] );
-     }
-    //below
-    var below = tileID['row'] +1;
-    if (below < tileCount) { 
-      toggleTile(  below * tileCount + tileID['col'] );
-    }
+    // var above = tileID['row'] -1;
+    // if (above >= 0) { 
+    //   // developer.log(above.toString());
+    //   toggleTile(  above * tileCount + tileID['col'] );
+    //  }
+    // //below
+    // var below = tileID['row'] +1;
+    // if (below < tileCount) { 
+    //   toggleTile(  below * tileCount + tileID['col'] );
+    // }
 
-    //left
-    var left = tileID['col'] -1;
-    if (left >= 0) { 
-      toggleTile( tileID['row'] * tileCount + left );
-    }
-    //right
-    var right = tileID['col'] + 1;
-    if (right < tileCount) { 
-      toggleTile( tileID['row']*tileCount + right );
-    }
+    // //left
+    // var left = tileID['col'] -1;
+    // if (left >= 0) { 
+    //   toggleTile( tileID['row'] * tileCount + left );
+    // }
+    // //right
+    // var right = tileID['col'] + 1;
+    // if (right < tileCount) { 
+    //   toggleTile( tileID['row']*tileCount + right );
+    // }
   }
 
-  void toggleTile( index) {
+  void toggleTile( tileID ) {
+     var index = tileID['row'] * tileCount + tileID['col'];
     if (tilesCreated) {
-      tileList[index].toggleMyself();
-    }
-    setState(() {
+      developer.log(".....DISPATCHING CHANGE......");
+      // ChangeNotification(myID: {tileID})..dispatch(context);
+      // tileList[index].toggleMyself();
+      keyList[index].currentState.toggleMe();
+
+     // developer.log("about to set the board state.");
+   //   setState(() {
+         //developer.log("Setting the board state.");
           boardList[index] = !boardList[index];
-    });
+         // developer.log("boardList:" + boardList.toString());
+   // });
+   }
+    else {
+      boardList[index] = !boardList[index];
+    }
+    
 
     // if (tilesCreated) {
     //   // tileList[index].widget.toggleMe();
@@ -71,21 +105,22 @@ class _BoardState extends State<Board> {
    // developer.log(boardList.toString());
   }
 
+
   @override
   Widget build(BuildContext context) {
-    clearBoard();
-    _sequence = new Sequence(sequenceLength, tileCount);
-    _sequence.generateRandomSequence(sequenceLength);
-    //developer.log(boardList.toString());
-    _sequence.touches.forEach((ID) => 
-       touchTile(ID)
-    );
-
+    developer.log("REBUILDING BOARD.");
     // developer.log(boardList.toString());
-    return buildRows();
+    return NotificationListener<TouchNotification> (
+      onNotification: (notification) {
+        // developer.log('Got notified: ' + notification.myID.toString());
+        touchTile(notification.myID);
+        return true;
+      },
+      child:buildRows() );
   }
 
   Widget buildRows() {
+    developer.log("/// in buildRows: ///");
     List rows = <Widget>[];
 
     for (var i = 0; i < tileCount; i++) {
@@ -102,9 +137,12 @@ class _BoardState extends State<Board> {
 
 // Dynamically build a whole row of tiles
     for (var i = 0; i < tileCount; i++) {
-      var box = Box(tileSize, rowNum, i, boardList[rowNum * tileCount + i], touchTile);
+     // developer.log("building.... boardlist item = " + boardList[rowNum * tileCount + i].toString());
+      GlobalKey<BoxState> key = GlobalKey();
+      var box = Box(key, tileSize, rowNum, i, boardList[rowNum * tileCount + i]);
       tiles.add(box);
       tileList.add(box);
+      keyList.add(key);
     }
     tilesCreated = true;
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: tiles);
--- lib/box.dart
+++ lib/box.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
 import 'dart:developer' as developer;
 
-class Box extends StatefulWidget {
-  final tileSize, rowNum, colNum, touchTile, initialState;
+class TouchNotification extends Notification {
+  final Object myID;
+
+  const TouchNotification({this.myID});
+}
+class ChangeNotification extends Notification {
+  final Object myID;
+
+  const ChangeNotification({this.myID});
+}
+
+class Box extends StatefulWidget  {
+  final key, tileSize, rowNum, colNum, initialState;
 
   //Box({Key key, onState}) : super(key: key);
-  Box(this.tileSize, this.rowNum, this.colNum, this.initialState,
-      this.touchTile);
+  Box(this.key, this.tileSize, this.rowNum, this.colNum, this.initialState);
 
   void toggleMyself() {
     developer.log("toggling myself.");
-
+    // toggleMe();
+    
   }
 
   @override
-  _BoxState createState() => _BoxState();
+  BoxState createState() => BoxState();
 }
 
-class _BoxState extends State<Box> {
+class BoxState extends State<Box>  {
   double endSize;
   bool onState;
 
@@ -53,7 +64,13 @@ class _BoxState extends State<Box> {
                 tween: Tween(begin: 0.0, end: endSize),
                 duration: Duration(milliseconds: 100),
                 builder: (_, num myWidth, __) {
-                  return Container(
+                  return NotificationListener<ChangeNotification>(
+                    onNotification: (notification) {
+                      developer.log('Got notified of change: ' + notification.myID.toString());
+                      toggleMe();
+                      return true;
+                    },
+                    child:Container(
                     alignment: Alignment.center,
                     width: myWidth,
                     height: myWidth,
@@ -67,8 +84,11 @@ class _BoxState extends State<Box> {
                             spreadRadius: 5),
                       ],
                     ),
+                  )
                   );
-                })));
+                })
+                )
+                );
   }
 
   void pressDown(details) {
@@ -84,8 +104,9 @@ class _BoxState extends State<Box> {
   }
 
   void pressUp(details) {
+    TouchNotification(myID: {'row':widget.rowNum, 'col': widget.colNum })..dispatch(context);
     setState(() {
-      widget.touchTile( { 'row': widget.rowNum, 'col': widget.colNum });
+      // widget.touchTile( { 'row': widget.rowNum, 'col': widget.colNum });
       endSize = widget.tileSize * .9;
     });
   }
